c3338301fa81cad3d3c6b6cf85105aedc05c824b
ewong1216 <31543324+ewong1216@users.noreply.github.com>
2019-04-14 17:24:58 -0700

    Slides 72-144
    72-76: Relative panel practice with the resizing purple and yellow rectangles.
    77-86: Relative panel practice search bar interface.
    88-95: Splitview practice with a button that toggles the pane over the content.
    103-128: Page navigation using multiple pages with buttons and passing information between pages.
    129-144: Input controls practice, checkbox, radio button, combobox, listbox, image, and togglebutton, all used to change the text of a corresponding textblock.

--- Lecture/Lecture/MainPage.xaml.cs
+++ Lecture/Lecture/MainPage.xaml.cs
@@ -25,12 +25,106 @@ namespace Lecture
         public MainPage()
         {
             this.InitializeComponent();
+            //InnerFrame.Navigate(typeof(Page1));
         }
 
-        private void Button1_Click(object sender, RoutedEventArgs e)
+        private void CheckBox1_Tapped(object sender, TappedRoutedEventArgs e)
         {
-            //textblock1.Text = "Hello World!";
+            if (CheckBox1.IsChecked.Value)
+            {
+                CheckBoxResultTextBlock.Text = "Yes";
+            }
+            else
+            {
+                CheckBoxResultTextBlock.Text = "No";
+            }
         }
 
+        private void Radio_Checked(object sender, RoutedEventArgs e)
+        {
+            if (YesRadio.IsChecked.Value)
+            {
+                RadioTextBlock.Text = "Yes";
+            }
+            else
+            {
+                RadioTextBlock.Text = "No";
+            }
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if(ComboBoxTextBlock != null)
+            {
+                ComboBox combo = (ComboBox)sender;
+                ComboBoxItem item = (ComboBoxItem)combo.SelectedItem;
+
+                ComboBoxTextBlock.Text = item.Content.ToString();
+            }
+        }
+
+        private void ListBox1_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            string s = "";
+            foreach(object item in ListBox1.Items)
+            {
+                ListBoxItem listboxitem = (ListBoxItem)item;
+                if (listboxitem.IsSelected)
+                {
+                    s += listboxitem.Content.ToString();
+                }
+            }
+
+            ListBoxTextBlock.Text = s;
+        }
+
+        private void ToggleButton1_Click(object sender, RoutedEventArgs e)
+        {
+            if (ToggleButton1.IsChecked.HasValue)
+            {
+                if (ToggleButton1.IsChecked.Value)
+                {
+                    ToggleButtonTextBlock.Text = "True";
+                }
+                else
+                {
+                    ToggleButtonTextBlock.Text = "False";
+                }
+            }
+            else
+            {
+                ToggleButtonTextBlock.Text = "Null";
+            }
+        }
+
+        /*
+        private void HomeButton_Click(object sender, RoutedEventArgs e)
+        {
+            InnerFrame.Navigate(typeof(Page1));
+        }
+
+        private void BackButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (InnerFrame.CanGoBack)
+            {
+                InnerFrame.GoBack();
+            }
+        }
+
+        private void ForwardButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (InnerFrame.CanGoForward)
+            {
+                InnerFrame.GoForward();
+            }
+        }
+
+        */
+        /*
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            sv.IsPaneOpen = !sv.IsPaneOpen;
+        }
+        */
     }
 }

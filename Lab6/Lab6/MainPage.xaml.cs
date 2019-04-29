1b6104b2ab6e17a2b56ab2b4448dfa70f2c670ad
ewong1216 <31543324+ewong1216@users.noreply.github.com>
2019-04-29 09:47:35 -0700

    Lab 6 Setup
    Slides 212-223: Setting up lab 6. Added model and viewmodel folders. Added FodyWeavers, pasted the Observations.cs page and changed ints & doubles to double?. Added MainPageViewModel.cs to ViewModel folder Updated the MainPage.xaml.cs to look like the slide.

--- Lab6/Lab6/MainPage.xaml.cs
+++ Lab6/Lab6/MainPage.xaml.cs
@@ -12,6 +12,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Lab6.ViewModels;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -22,9 +23,19 @@ namespace Lab6
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        public MainPageViewModel ViewModel { get; set; } = new MainPageViewModel();
+
         public MainPage()
         {
             this.InitializeComponent();
+        }
+
+        protected async override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            ViewModel.Description = "Dummy Description";
+            ViewModel.LocationName = "Dummy Location";
+            ViewModel.Temperature = "Dummy Temperature";
+            ViewModel.ImageUrl = "https://icons.wxug.com/i/c/c/snow.gif";
         }
     }
 }

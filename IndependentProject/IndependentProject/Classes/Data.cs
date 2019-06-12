06052e7339ff663be80e82002d508d393136b3a9
ewong1216 <31543324+ewong1216@users.noreply.github.com>
2019-06-11 23:32:23 -0700

    Finishing Touches
    Balanced some costs for upgrades, named some unnamed upgrades.

--- IndependentProject/IndependentProject/Classes/Data.cs
+++ IndependentProject/IndependentProject/Classes/Data.cs
@@ -64,9 +64,9 @@ namespace IndependentProject
         private void AddHelpers()
         {
             Helpers.Add(new Helper("Auto Clicker", "Takes some clicks off your fingers", 1, 10, 1, 1));
-            Helpers.Add(new Helper("Monkeys", "Train monkeys to click for you!", 15, 800, 100, 60));
+            Helpers.Add(new Helper("Monkeys", "Train monkeys to click for you!", 15, 800, 100, 50));
             Helpers.Add(new Helper("Ethernet", "Connects your device through ethernet to speed up commits", 500, 50000, 12000, 10000));
-            Helpers.Add(new Helper("Office Building", "Concentrates monkeys into buildings instead of being spread out", 13000, 25000000, 50000, 40000));
+            Helpers.Add(new Helper("Office Building", "Concentrates monkeys into buildings instead of being spread out", 13000, 25000000, 600000, 40000));
             Helpers.Add(new Helper("Satellites", "Take control of innocent people's devices for comitting purposes", 900000, 1000000000, 5000000, 20000));
         }
         private void AddUpgrades()
@@ -81,20 +81,20 @@ namespace IndependentProject
             Upgrades.Add(new Upgrade("/Assets/monkey.jpg", 50000, 0.5, "Acrobat Training", "Increases Monkey CPS by 50%", Helpers.ElementAt(1)));
             Upgrades.Add(new Upgrade("/Assets/monkey.jpg", 75000, 1.0, "Genetic Modification", "Increases Monkey CPS by 100%", Helpers.ElementAt(1)));
 
-            Upgrades.Add(new Upgrade("/Assets/cable.png", 5000000, 0.1, "Better Installation", "Increases Ethernet CPS by 10%", Helpers.ElementAt(2)));
-            Upgrades.Add(new Upgrade("/Assets/cable.png", 15000000, 0.25, "More Connected Devices", "Increases Ethernet CPS by 25%", Helpers.ElementAt(2)));
-            Upgrades.Add(new Upgrade("/Assets/cable.png", 30000000, 0.5, "Server Control", "Increases Ethernet CPS by 50%", Helpers.ElementAt(2)));
+            Upgrades.Add(new Upgrade("/Assets/cable.png", 1000000, 0.1, "Better Installation", "Increases Ethernet CPS by 10%", Helpers.ElementAt(2)));
+            Upgrades.Add(new Upgrade("/Assets/cable.png", 7000000, 0.25, "More Connected Devices", "Increases Ethernet CPS by 25%", Helpers.ElementAt(2)));
+            Upgrades.Add(new Upgrade("/Assets/cable.png", 25000000, 0.5, "Server Control", "Increases Ethernet CPS by 50%", Helpers.ElementAt(2)));
             Upgrades.Add(new Upgrade("/Assets/cable.png", 60000000, 1.0, "Market Monopoly", "Increases Ethernet CPS by 100%", Helpers.ElementAt(2)));
 
-            Upgrades.Add(new Upgrade("/Assets/office.jpg", 6000000, 0.1, "Clean Bathrooms", "Increases Office Building CPS by 10%", Helpers.ElementAt(3)));
-            Upgrades.Add(new Upgrade("/Assets/office.jpg", 11000000, 0.25, "Temperature Control", "Increases Office Building CPS by 25%", Helpers.ElementAt(3)));
-            Upgrades.Add(new Upgrade("/Assets/office.jpg", 500000000, 0.5, "Basement Expansion", "Increases Office Building CPS by 50%", Helpers.ElementAt(3)));
-            Upgrades.Add(new Upgrade("/Assets/office.jpg", 900000000, 1.0, "Skyscrapers", "Increases Office Building CPS by 100%", Helpers.ElementAt(3)));
+            Upgrades.Add(new Upgrade("/Assets/office.jpg", 200000000, 0.1, "Clean Bathrooms", "Increases Office Building CPS by 10%", Helpers.ElementAt(3)));
+            Upgrades.Add(new Upgrade("/Assets/office.jpg", 800000000, 0.25, "Air Conditioning", "Increases Office Building CPS by 25%", Helpers.ElementAt(3)));
+            Upgrades.Add(new Upgrade("/Assets/office.jpg", 1000000000, 0.5, "Basement Expansion", "Increases Office Building CPS by 50%", Helpers.ElementAt(3)));
+            Upgrades.Add(new Upgrade("/Assets/office.jpg", 9000000000, 1.0, "Skyscrapers", "Increases Office Building CPS by 100%", Helpers.ElementAt(3)));
 
             Upgrades.Add(new Upgrade("/Assets/satellite.jpg", 8000000000, 0.1, "Cheaper Materials", "Increases Satellite CPS by 10%", Helpers.ElementAt(4)));
-            Upgrades.Add(new Upgrade("/Assets/satellite.jpg", 15000000000, 0.25, "", "Increases Satellite CPS by 25%", Helpers.ElementAt(4)));
-            Upgrades.Add(new Upgrade("/Assets/satellite.jpg", 60000000000, 0.5, "", "Increases Satellite CPS by 50%", Helpers.ElementAt(4)));
-            Upgrades.Add(new Upgrade("/Assets/satellite.jpg", 100000000000, 1.0, "", "Increases Satellite CPS by 100%", Helpers.ElementAt(4)));
+            Upgrades.Add(new Upgrade("/Assets/satellite.jpg", 15000000000, 0.25, "Improved Solar Panels", "Increases Satellite CPS by 25%", Helpers.ElementAt(4)));
+            Upgrades.Add(new Upgrade("/Assets/satellite.jpg", 60000000000, 0.5, "Space Tech Revolution", "Increases Satellite CPS by 50%", Helpers.ElementAt(4)));
+            Upgrades.Add(new Upgrade("/Assets/satellite.jpg", 100000000000, 1.0, "Intergalactic Dyson Spheres", "Increases Satellite CPS by 100%", Helpers.ElementAt(4)));
         }
 
         private void AddAchievements()
@@ -107,7 +107,7 @@ namespace IndependentProject
             Achievements.Add(new Achievement("Commit Investigator", 200, 10000000000, "Earn 10B commits all time", "/Assets/mouse.png", 0, this, null));
             Achievements.Add(new Achievement("Commit Researcher", 400, 1000000000000, "Earn 1T commits all time", "/Assets/mouse.png", 0, this, null));
 
-            Achievements.Add(new Achievement("Quick Committer", 5, 1, "Reach 10 CPS", "/Assets/upload.png", 2, this, null));
+            Achievements.Add(new Achievement("Quick Committer", 5, 10, "Reach 10 CPS", "/Assets/upload.png", 2, this, null));
             Achievements.Add(new Achievement("Swift Committer", 10, 100, "Reach 100 CPS", "/Assets/upload.png", 2, this, null));
             Achievements.Add(new Achievement("Rapid Committer", 30, 1000, "Reach 1,000 CPS", "/Assets/upload.png", 2, this, null));
             Achievements.Add(new Achievement("Hyper Committer", 50, 10000, "Reach 10,000 CPS", "/Assets/upload.png", 2, this, null));
@@ -137,19 +137,19 @@ namespace IndependentProject
             Achievements.Add(new Achievement("Ethernet Legend", 100, 500, "Reach Ethernet Level 500", "/Assets/cable.png", 1, this, Helpers.ElementAt(2)));
             Achievements.Add(new Achievement("Ethernet God", 200, 1000, "Reach Ethernet Level 1000", "/Assets/cable.png", 1, this, Helpers.ElementAt(2)));
 
-            Achievements.Add(new Achievement("Office Building Beginner", 5, 1, "Reach Office Building Level 1", "/Assets/office.jpg", 1, this, Helpers.ElementAt(3)));
-            Achievements.Add(new Achievement("Office Building Novice", 10, 10, "Reach Office Building Level 10", "/Assets/office.jpg", 1, this, Helpers.ElementAt(3)));
-            Achievements.Add(new Achievement("Office Building Trainee", 20, 25, "Reach Office Building Level 25", "/Assets/office.jpg", 1, this, Helpers.ElementAt(3)));
-            Achievements.Add(new Achievement("Office Building Master", 50, 100, "Reach Office Building Level 100", "/Assets/office.jpg", 1, this, Helpers.ElementAt(3)));
-            Achievements.Add(new Achievement("Office Building Legend", 100, 500, "Reach Office Building Level 500", "/Assets/office.jpg", 1, this, Helpers.ElementAt(3)));
-            Achievements.Add(new Achievement("Office Building God", 200, 1000, "Reach Office Building Level 1000", "/Assets/office.jpg", 1, this, Helpers.ElementAt(3)));
+            Achievements.Add(new Achievement("Office Building Beginner", 10, 1, "Reach Office Building Level 1", "/Assets/office.jpg", 1, this, Helpers.ElementAt(3)));
+            Achievements.Add(new Achievement("Office Building Novice", 20, 10, "Reach Office Building Level 10", "/Assets/office.jpg", 1, this, Helpers.ElementAt(3)));
+            Achievements.Add(new Achievement("Office Building Trainee", 50, 25, "Reach Office Building Level 25", "/Assets/office.jpg", 1, this, Helpers.ElementAt(3)));
+            Achievements.Add(new Achievement("Office Building Master", 70, 100, "Reach Office Building Level 100", "/Assets/office.jpg", 1, this, Helpers.ElementAt(3)));
+            Achievements.Add(new Achievement("Office Building Legend", 120, 500, "Reach Office Building Level 500", "/Assets/office.jpg", 1, this, Helpers.ElementAt(3)));
+            Achievements.Add(new Achievement("Office Building God", 280, 1000, "Reach Office Building Level 1000", "/Assets/office.jpg", 1, this, Helpers.ElementAt(3)));
 
-            Achievements.Add(new Achievement("Satellite Beginner", 5, 1, "Reach Satellite Level 1", "/Assets/satellite.jpg", 1, this, Helpers.ElementAt(4)));
-            Achievements.Add(new Achievement("Satellite Novice", 10, 10, "Reach Satellite Level 10", "/Assets/satellite.jpg", 1, this, Helpers.ElementAt(4)));
-            Achievements.Add(new Achievement("Satellite Trainee", 20, 25, "Reach Satellite Level 25", "/Assets/satellite.jpg", 1, this, Helpers.ElementAt(4)));
-            Achievements.Add(new Achievement("Satellite Master", 50, 100, "Reach Satellite Level 100", "/Assets/satellite.jpg", 1, this, Helpers.ElementAt(4)));
-            Achievements.Add(new Achievement("Satellite Legend", 100, 500, "Reach Satellite Level 500", "/Assets/satellite.jpg", 1, this, Helpers.ElementAt(4)));
-            Achievements.Add(new Achievement("Satellite God", 200, 1000, "Reach Satellite Level 1000", "/Assets/satellite.jpg", 1, this, Helpers.ElementAt(4)));
+            Achievements.Add(new Achievement("Satellite Beginner", 20, 1, "Reach Satellite Level 1", "/Assets/satellite.jpg", 1, this, Helpers.ElementAt(4)));
+            Achievements.Add(new Achievement("Satellite Novice", 50, 10, "Reach Satellite Level 10", "/Assets/satellite.jpg", 1, this, Helpers.ElementAt(4)));
+            Achievements.Add(new Achievement("Satellite Trainee", 80, 25, "Reach Satellite Level 25", "/Assets/satellite.jpg", 1, this, Helpers.ElementAt(4)));
+            Achievements.Add(new Achievement("Satellite Master", 150, 100, "Reach Satellite Level 100", "/Assets/satellite.jpg", 1, this, Helpers.ElementAt(4)));
+            Achievements.Add(new Achievement("Satellite Legend", 200, 500, "Reach Satellite Level 500", "/Assets/satellite.jpg", 1, this, Helpers.ElementAt(4)));
+            Achievements.Add(new Achievement("Satellite God", 400, 1000, "Reach Satellite Level 1000", "/Assets/satellite.jpg", 1, this, Helpers.ElementAt(4)));
         }
 
         private void AddSpecials()
